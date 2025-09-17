ae594aa794868af079f0dc9d9caa76624331b296
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-17 19:17:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BiluthrningExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Jag har brainstormat, samlat data och fått information av ett scenario.
+           //Jag ska skriva kod om ett biluthyrningsföretag med flera classer.
+
+
+        }
+    }
+}
