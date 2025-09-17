5979d8fc9eb09be2fdb394ff7efb04157ae9d150
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-17 19:38:55 +0200

    Lagt till en class med biluthyrningsföretag med info om företaget. Även lagt in en bild på planeringen av projektet.

---
+++ Classes/Biluthyrning.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BiluthrningExercise.Classes
+{
+    public class CarRent
+    {
+        public string Name;
+        public string Adress;
+        public string OpenHours;
+        public int Contact;
+        
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($" Welocome to {Name} at {Adress} everyday {OpenHours}, feel free to call us at {Contact} ");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BiluthrningExercise
+﻿using BiluthrningExercise.Classes;
+
+namespace BiluthrningExercise
 {
     internal class Program
     {
@@ -6,6 +8,16 @@
         {
            //Jag har brainstormat, samlat data och fått information av ett scenario.
            //Jag ska skriva kod om ett biluthyrningsföretag med flera classer.
+
+
+           CarRent company = new CarRent();
+            company.Name = "HB Luxury";
+            company.Adress = "Göteborg";
+            company.OpenHours = "09:00 - 17:00";
+            company.Contact = 123456789;
+
+            company.DisplayInfo();
+
 
 
         }
