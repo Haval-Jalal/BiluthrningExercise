270247abdc0331a3a9c3aa53b8de1d2ab991c0f5
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-21 20:31:05 +0200

    Skapat en ny klass, filen heter Fordon och själva klassen heter Vechicle

---
+++ Classes/Fordon.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace BiluthrningExercise.Classes
+{
+    public abstract class Vehicle
+    {
+        public string Type;
+        public string Brand;
+        public string Model;
+        public int Year;
+        public int PeriodInDays;
+        public int Price;
+
+    }
+}
