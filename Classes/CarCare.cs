c4bd1232e30c833aba45018261f249efb03bf55f
Haval-Jalal <Haval.jalal@hotmail.com>
2025-09-21 21:25:18 +0200

    Skapat en ny klass med Maintance

---
+++ Classes/CarCare.cs
@@ -0,0 +1,20 @@
+﻿namespace BiluthrningExercise.Classes
+{
+    public class Maintance
+    {
+        public string MaintanceNeed;
+        public string MaintacnceHistory;
+
+
+        public void NextMaintance()
+        {
+            Console.WriteLine("The next maintance of this vehicle is in 7 months");
+        }
+
+
+        public void LastMaintance()
+        {
+            Console.WriteLine("The last maintance of this vehicle was 3 months ago");
+        }
+    }
+}
